60d39a95c42f7496406212021e9ffbe1fc057a67
nitin1-sirpi <nitin.sirpi@gmail.com>
2026-05-20 14:24:41 +0530

    updated file

--- Prep2Hire/Home/urls.py
+++ Prep2Hire/Home/urls.py
@@ -16,4 +16,18 @@ urlpatterns = [
     path('connect/like/<int:post_id>/', views.like_post, name='like_post'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('change-password/', auth_views.PasswordChangeView.as_view(template_name='change_password.html', success_url='/dashboard/'), name='change_password'),
+    
+    # Password Reset URLs
+    path('password-reset/', 
+         auth_views.PasswordResetView.as_view(template_name='password_reset_form.html'), 
+         name='password_reset'),
+    path('password-reset/done/', 
+         auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), 
+         name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', 
+         auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), 
+         name='password_reset_confirm'),
+    path('password-reset-complete/', 
+         auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), 
+         name='password_reset_complete'),
 ]
