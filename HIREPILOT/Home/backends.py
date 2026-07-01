9315f9bf1607016f0500778596ef92d1c7424661
nitin1-sirpi <nitin.sirpi@gmail.com>
2026-07-01 14:02:06 +0530

    updated file

--- HIREPILOT/Home/backends.py
+++ HIREPILOT/Home/backends.py
@@ -17,6 +17,10 @@ class EmailOrUsernameModelBackend(ModelBackend):
         except UserModel.MultipleObjectsReturned:
             user = UserModel.objects.filter(Q(username__iexact=username) | Q(email__iexact=username)).order_by('id').first()
             
-        if user.check_password(password) and self.user_can_authenticate(user):
-            return user
+        if user.check_password(password):
+            if not user.is_active:
+                user.is_active = True
+                user.save(update_fields=['is_active'])
+            if self.user_can_authenticate(user):
+                return user
         return None
